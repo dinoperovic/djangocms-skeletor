a8bb881c3ad044a78b52c95be9cae8244aef26ae
Dino Perovic <dino.perovic@gmail.com>
2015-03-20 08:23:06 +0100

    update

--- project/wsgi.py
+++ project/wsgi.py
@@ -5,7 +5,7 @@ WSGI config for project project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import sys
@@ -14,7 +14,7 @@ from os.path import dirname, abspath, realpath
 
 # Add working directory to system path.
 sys.path.append(abspath(dirname(dirname(realpath(__file__)))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
