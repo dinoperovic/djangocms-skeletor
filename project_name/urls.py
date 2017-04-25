e9c26e38b38f1161fff2fb3ab9ee7eec9b22a0a2
Dino Perovic <dino.perovic@gmail.com>
2017-04-25 11:40:50 +0200

    remove solid-i18n

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from solid_i18n.urls import solid_i18n_patterns as i18n_patterns
 
 sitemaps = {
     'cmspages': CMSSitemap,
@@ -34,4 +34,5 @@ urlpatterns += i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^translate/', include('rosetta.urls')),
     url(r'^', include('cms.urls')),
+    prefix_default_language=False,
 )
