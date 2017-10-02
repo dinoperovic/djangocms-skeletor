71a23b2a0c1955a467d9638f9664196cf9f2ab1a
Dino Perovic <dino.perovic@gmail.com>
2017-10-02 10:28:47 +0200

    change picture save function

--- project_name/settings.py
+++ project_name/settings.py
@@ -218,7 +218,7 @@ class Common(Configuration):
         ],
         'skin': 'moono',
     }
-    TEXT_SAVE_IMAGE_FUNCTION = 'cmsplugin_filer_image.integrations.ckeditor.create_image_plugin'
+    TEXT_SAVE_IMAGE_FUNCTION = 'djangocms_text_ckeditor.picture_save.create_picture_plugin'
 
 
 class Development(Common):
